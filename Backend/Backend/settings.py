863966fdda37c57cfc471a91c1454e9a6c82c09c
ismath <ismathrm9@.com>
2024-08-23 20:12:38 +0530

    env change

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -112,9 +112,12 @@ DATABASES = {
 # }
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
 
 
