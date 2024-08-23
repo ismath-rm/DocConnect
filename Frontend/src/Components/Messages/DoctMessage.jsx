caba4671270e0e4be46afc4e0b586eca61fd3995
ismath <ismathrm9@.com>
2024-08-23 19:17:02 +0530

    env change7

--- Frontend/src/Components/Messages/DoctMessage.jsx
+++ Frontend/src/Components/Messages/DoctMessage.jsx
@@ -48,8 +48,7 @@ const DoctorChatComponent = () => {
   };
 
   const fetchDoctorID = (id) => {
-    UserAPIwithAcess
-      .get(BASE_URL + `0auth/custom-id/doctor/${id}`, config)
+    UserAPIwithAcess.get(BASE_URL + `auth/custom-id/doctor/${id}`, config)
       .then((res) => {
         setdoct(res.data);
         console.log(res.data.doctor_user.custom_id);
