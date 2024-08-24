a603edf014d849823eeba9cea36eff49148d092f
ismath <ismathrm9@.com>
2024-08-24 12:50:14 +0530

    changes in asgi

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -1,11 +1,9 @@
 import os
-import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from .urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
