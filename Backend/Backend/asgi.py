7adad84f2b1a38f96dc8b6fed385c04c316e1ec8
ismath <ismathrm9@.com>
2024-08-24 13:20:05 +0530

    Changed in asgi1

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application   
 from .urls import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
-django.setup()
 
 application = get_asgi_application()
 
