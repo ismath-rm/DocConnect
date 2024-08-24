a48c4f92ee87c7e70146539796ef11f7edaa0f7a
ismath <ismathrm9@.com>
2024-08-24 13:04:32 +0530

    Changed in asgi

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -1,11 +1,26 @@
+"""
+ASGI config for Backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application   
 from .urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+django.setup()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
-})
+application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": application,
+        "websocket": URLRouter(websocket_urlpatterns),
+    }
+)
